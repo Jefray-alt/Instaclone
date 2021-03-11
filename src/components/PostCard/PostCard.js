fe654bf4349a8134114c419607460257694b9c0c
unknown <jbfrance1626@gmail.com>
2021-03-11 22:10:19 +0800

    initialized layout and cards

---
+++ src/components/PostCard/PostCard.js
@@ -0,0 +1,30 @@
+import { Card, CardBody } from '@windmill/react-ui'
+import React from 'react'
+
+const PostCard = () => {
+	return (
+		<Card>
+			<div className='shadow-lg px-2 py-3 flex items-center'>
+				<img
+					src='https://via.placeholder.com/40'
+					alt='sample'
+					className='rounded-full'
+				/>
+				<span className='ml-3'>Name</span>
+			</div>
+			<img src='https://via.placeholder.com/512' alt='sample' />
+			<CardBody>
+				<div className='flex flex-col'>
+					<a href='#' className='font-bold'>
+						11 Likes
+					</a>
+					<a href='#' className='text-gray-500'>
+						View 1 comment(s)
+					</a>
+				</div>
+			</CardBody>
+		</Card>
+	)
+}
+
+export default PostCard
